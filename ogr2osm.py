747d955d03e5a0f6bdd53ab5455d0d765fce8ce6
Andrew Guertin <andrew.guertin@uvm.edu>
2012-02-28 10:27:09 -0500

    Fix another fatal error with MultiPolygon parsing
    Reported-by: Kristoffer Malmström <mitod1@gmail.com>

--- ogr2osm.py
+++ ogr2osm.py
@@ -396,7 +396,7 @@ def parseCollection(ogrgeometry):
     if (geometryType == ogr.wkbMultiPolygon or
         geometryType == ogr.wkbMultiPolygon25D):
         geometry = Relation()
-        for polygon in ogrgeometry.GetGeometryCount():
+        for polygon in range(ogrgeometry.GetGeometryCount()):
             exterior = parseLineString(ogrgeometry.GetGeometryRef(polygon).GetGeometryRef(0))
             geometry.members.append((exterior, "outer"))
             for i in range(1, ogrgeometry.GetGeometryRef(polygon).GetGeometryCount()):
